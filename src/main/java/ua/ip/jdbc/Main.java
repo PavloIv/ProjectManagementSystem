a91be8ff04b71bfc8a6aa3524f9d800a72b48ac0
PavloIv <95400105+PavloIv@users.noreply.github.com>
2022-09-20 13:13:16 +0300

    finish commit for first audit

--- src/main/java/ua/ip/jdbc/Main.java
+++ src/main/java/ua/ip/jdbc/Main.java
@@ -1,21 +1,17 @@
 package ua.ip.jdbc;
 
 import ua.ip.jdbc.cli.CliFSM;
-import ua.ip.jdbc.dao.CustomerDao;
-import ua.ip.jdbc.dao.DevelopersDao;
-import ua.ip.jdbc.dao.ProjectsDao;
+import ua.ip.jdbc.storage.DatabaseInitService;
 import ua.ip.jdbc.storage.DatabaseSqlManagerConnector;
-import ua.ip.jdbc.table.Projects;
 
 import java.sql.SQLException;
-import java.time.LocalDate;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
-        DatabaseSqlManagerConnector sqlConnector = new DatabaseSqlManagerConnector("localhost","5432",
-              "GrafProductCompany","postgres", "grafmk1523");
-//      new DatabaseInitService().initDb(sqlConnector.getUrl(),sqlConnector.getUser(),sqlConnector.getPassword());
+        DatabaseSqlManagerConnector sqlConnector = new DatabaseSqlManagerConnector("localhost", "5432",
+                    "GrafProductCompany", "postgres", "grafmk1523");
+        new DatabaseInitService().initDb(sqlConnector.getUrl(), sqlConnector.getUser(), sqlConnector.getPassword());
 
         new CliFSM(sqlConnector);
 
